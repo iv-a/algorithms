c5b667b1520d5a5f212fb77a5abac0f681c15e44
Artem Ivanov <ivart.dev@gmail.com>
2021-08-08 10:55:00 +0300

    theory: add B_Is-prime-optimized

---
+++ 01_Introduction-to-algorithms/Theory/Prime-numbers-search/B_Is-prime-optimized.js
@@ -0,0 +1,13 @@
+function isPrimeOptimized(n) {
+  if (n === 1) {
+    return false;
+  }
+  let i = 2;
+  while (i * i < n) {
+    if (n % i === 0) {
+      return false;
+    }
+    i++;
+  }
+  return true;
+}
