5570a12b4b3c4ba218c1231250cc5e4c50dd381e
Artem Ivanov <ivart.dev@gmail.com>
2021-08-08 11:12:00 +0300

    theory: add D_Sieve-of-Eratosthenes

---
+++ 01_Introduction-to-algorithms/Theory/Prime-numbers-search/D_Sieve-of-Eratosthenes.js
@@ -0,0 +1,51 @@
+const _readline = require('readline');
+const DEC = 10;
+
+const _io_interface = _readline.createInterface({
+  input: process.stdin
+});
+
+let _curLine = 0;
+const _inputLines = [];
+
+_io_interface.on('line', (line) => {
+  _inputLines.push(line);
+  if (_inputLines.length === 1) {
+    _io_interface.close();
+  }
+})
+
+_io_interface.on('close', () => {
+  process.stdout.write(solve());
+});
+
+function createArray(n) {
+  const arr = [];
+  for (let i = 0; i < n; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+function eratosthenes(n) {
+  const numbers = createArray(n);
+  numbers[0] = numbers[1] = false;
+  for (let num = 2; num < n; num++) {
+    if (numbers[num]) {
+      for (let j = 2 * num; j < n + 1; j += num) {
+        numbers[j] = false;
+      }
+    }
+  }
+  return numbers.join(' ');
+}
+
+function solve() {
+  const number = readInt();
+
+  return eratosthenes(number);
+}
+
+function readInt() {
+  return parseInt(_inputLines[_curLine++], DEC);
+}
